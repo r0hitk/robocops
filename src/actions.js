e8da9b578886d7f79154a325bc3e9da6feed2bd8
Rohit Kumar <59209227+r0hitk@users.noreply.github.com>
2021-04-09 02:49:37 +0530

    changed an action creator

--- src/actions.js
+++ src/actions.js
@@ -1,20 +1,25 @@
-import {CHANGE_SEARCH_FIELD,
-    ROBOT_REQUEST_SUCCESS,
-    ROBOT_REQUEST_FAILED,
-    ROBOT_REQUEST_PENDING} from "./constants";
+import {
+  CHANGE_SEARCH_FIELD,
+  ROBOT_REQUEST_SUCCESS,
+  ROBOT_REQUEST_FAILED,
+  ROBOT_REQUEST_PENDING,
+} from "./constants";
 
-export const setSearchBox = text => {
-return {
-        type: CHANGE_SEARCH_FIELD,
-        payload: text
-    };    
-}
+export const setSearchBox = (text) => {
+  return {
+    type: CHANGE_SEARCH_FIELD,
+    payload: text,
+  };
+};
 
-export const requestRobots = () => dispatch => {
-
-    dispatch({type: ROBOT_REQUEST_PENDING});
+export const requestRobots = () => {
+  return (dispatch) => {
+    dispatch({ type: ROBOT_REQUEST_PENDING });
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(data => dispatch({type: ROBOT_REQUEST_SUCCESS, payload: data}))
-    .catch(error => dispatch({type: ROBOT_REQUEST_FAILED, payload: error}));
-}
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: ROBOT_REQUEST_SUCCESS, payload: data }))
+      .catch((error) =>
+        dispatch({ type: ROBOT_REQUEST_FAILED, payload: error })
+      );
+  };
+};
